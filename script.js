480c2cacbda98b1fefc16a07d11f204b0af51970
xahio <150611129+xahio@users.noreply.github.com>
2024-06-12 20:40:18 +0200

    .

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ buttons.forEach(button => {
         button.style.transform = 'scale(1)';
     });
 });
+
+const video = document.getElementById("myVideo");
+const soundsButton = document.getElementById("soundsButton");
+let isMuted = true; // Zmienna śledząca stan wyciszenia
+
+soundsButton.addEventListener("click", function() {
+    if (isMuted) {
+        video.muted = false; // Włącz dźwięk
+        isMuted = false; // Zaktualizuj stan wyciszenia
+        soundsButton.textContent = "Mute"; // Zaktualizuj tekst przycisku
+    } else {
+        video.muted = true; // Wycisz dźwięk
+        isMuted = true; // Zaktualizuj stan wyciszenia
+        soundsButton.textContent = "UnMute"; // Zaktualizuj tekst przycisku
+    }
+});
